41904c3b993c450ff3017489cac277c36b081229
Austin Lancaster <alancaster67@gmail.com>
2023-12-06 14:16:17 -0700

    Completed batchSize+LR experiment and updated the config file with the best combination of BS and LR that was discovered.

---
+++ ModelCreation/ExperimentAnalysis.py
@@ -0,0 +1,5 @@
+#! C:\Users\alanc\OneDrive\Documents\Python Projects\DogWaterLevelDetector\ModelCreation\venv\Scripts\python.exe
+from pathlib import Path
+p = Path('.')
+print(p)
+print(list(p.glob('**/1LConvSize/')))
